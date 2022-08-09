19721214ff7a94afdb6a9ea440dd219d8be7e333
igor <igorbelikov2009@yandex.ru>
2022-08-09 19:08:00 +0300

    userefhook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-// import UseEffectHook from "./components/UseEffectHook.tsx";
 
 function App() {
-  return <div>{/* <UseEffectHook /> */}</div>;
+  return <div>hello</div>;
 }
 
 export default App;
---
+++ src/components/UseRefHook.tsx
@@ -0,0 +1,43 @@
+// import UseEffectHook from "./components/UseEffectHook.tsx";
+import React, { useEffect, useRef, useState } from "react";
+
+function UseRefHook() {
+  // const [renderCount, setRenderCount] = useState(1);
+  const [value, setValue] = useState("initial");
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef("");
+
+  function focus() {
+    inputRef.current.focus();
+  }
+
+  useEffect(() => {
+    renderCount.current++;
+    console.log(inputRef.current.value);
+  });
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value]);
+
+  return (
+    <div>
+      <h1>Количество рендеров: {renderCount.current}</h1>
+      <h2>Прошлое состояние: {prevValue.current}</h2>
+
+      <input
+        ref={inputRef}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+
+      <button className="btn btn-success" onClick={focus}>
+        Фокус
+      </button>
+    </div>
+  );
+}
+
+export default UseRefHook;
