a06187e6390740c17a18e1392c08dfa612b09c4c
igor <igorbelikov2009@yandex.ru>
2022-08-11 14:00:51 +0300

    useInput

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
-import UseContextHook from "./components/UseContextHook.tsx";
+import React, { useEffect, useState } from "react";
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log("value changed", value);
+  }, [value]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  return {
+    value,
+    onChange,
+  };
+}
 
 function App() {
+  const input = useInput("");
+  const lastName = useInput("");
+
+  useLogger(input.value);
+
   return (
-    <div>
-      <UseContextHook />
+    <div className="container pt-3">
+      {/* <input type="text" value={input.value} onChange={input.onChange} /> */}
+      <input type="text" {...input} />
+      <input type="text" {...lastName} />
+
+      <hr />
+      <h3>
+        {input.value} {lastName.value}{" "}
+      </h3>
     </div>
   );
 }
--- src/components/UseContextHook.tsx
+++ src/components/UseContextHook.tsx
@@ -6,7 +6,7 @@ import AlertProvider from "../alert/AlertContext";
 function UseContextHook() {
   return (
     <AlertProvider>
-      <div className="container pt-3">
+      <div>
         <h1 className="text-primary">UseContext</h1>
         <Alert />
 
